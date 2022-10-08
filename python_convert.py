c4550ad2cd6d7baa0c8b2a67454d47fd1b2f2edc
Faiza4265 <98763781+Faiza4265@users.noreply.github.com>
2022-10-08 12:03:26 +0300

    Update python_convert.py

--- python_convert.py
+++ python_convert.py
@@ -1,22 +1,27 @@
-import pandas as pd
+import pandas as pd #import pandas inorder to put the result in a dataframe
 
 list1=[]
 
-for i in range (0,6):
+for i in range (0,6): #create a loop using for loop for reciving input of grade result from a user
     x=int(input())
     list1.append(x)
 list1
 
 
-def funk(list1):
+def funk(list1): #create a function that returns the mathimathical formula or equation we want to apply 
     return list1*40/30
     
     
-result=list(map(funk,list1))  
+result=list(map(funk,list1)) #then mapping the input list with the mathimathical formula function for new result
      
-x={ "input": list1, "converted": result
+x={ "input": list1, "converted": result 
                      }     
 
-put_dataframe=pd.DataFrame(data=x)
-put_dataframe
+put_dataframe=pd.DataFrame(data=x) # create a dataframe inorder to display the result in a more visible way
+put_dataframe # at the end print out the result
+
+
+
+
+
 
